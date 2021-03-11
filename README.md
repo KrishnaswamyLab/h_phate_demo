8265e239d955053e38535066d809ea26414567e6
Scott Gigante <scottgigante@gmail.com>
2021-03-11 12:18:28 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# H-PHATE Demo
+
+This is the repository used to generate and host the demonstration of Hierarchical PHATE.
+
+Click here to view the interactive notebooks on Binder: [PBMCs](https://mybinder.org/v2/gh/KrishnaswamyLab/h_phate_demo/master?filepath=H-PHATE_PBMC_Demo.ipynb), [Embryoid Body](https://mybinder.org/v2/gh/KrishnaswamyLab/h_phate_demo/master?filepath=H-PHATE_EB_Demo.ipynb).
---
+++ setup.py
@@ -0,0 +1,71 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+install_requires = [
+    'numpy>=1.10.0',
+    'scipy>=0.18.0',
+    'scikit-learn>=0.22.0',
+    'pandas>=0.25',
+    'imageio',
+    'pygsp',
+    'scprep',
+    'graphtools',
+    'phate',
+    'umap-learn',
+    'networkx',
+    'ipywidgets',
+    'plotly==3.10.0',
+    'numba',
+    'markdown',
+    'matplotlib>=3.0'
+]
+
+extras_requires = [
+    'h5py',
+    'rpy2>=3.0',
+]
+
+package_name = "blog_tools"
+
+if sys.version_info[:2] < (3, 6):
+    raise RuntimeError("Python version >=3.6 required.")
+
+version_py = os.path.join(os.path.dirname(
+    __file__), package_name, 'version.py')
+version = open(version_py).read().strip().split(
+    '=')[-1].replace('"', '').strip()
+
+readme = open('README.md').read()
+
+setup(name=package_name,
+      version=version,
+      description=package_name,
+      author='Scott Gigante and Daniel Burkhardt, Krishnaswamy Lab, Yale University',
+      author_email='krishnaswamylab@gmail.com',
+      packages=find_packages(),
+      license='GNU General Public License Version 2',
+      install_requires=install_requires,
+      long_description=readme,
+      url='https://github.com/KrishnaswamyLab/visualization_selection',
+      download_url="https://github.com/KrishnaswamyLab/visualization_selection/archive/v{}.tar.gz".format(
+          version),
+      keywords=['big-data',
+                'dimensionality-reduction',
+                ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Framework :: Jupyter',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering :: Visualization',
+      ]
+      )
